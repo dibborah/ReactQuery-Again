348a610ff49098da5f5fa766e5bf1ada87538560
dibborah <dibyajyotiborah469@gmail.com>
2024-01-24 22:12:40 +0530

    refetchOnWindowFocus , refetchOnReconnect, retry Queries

---
+++ src/notes2.js
@@ -0,0 +1,16 @@
+// set Refetch Defaults
+// QueryClient.defaultOptions = {
+//     queries: {
+//         refetchOnWindowFocus: false, // Disable refetching when the window regains focus
+//         refetchOnReconnect: false, // Disable refetching when the network connection is restored
+//         retry: 3, // Number of times to retry a failed request
+//         staleTime: 60000, // Data will be considered stale after 60 seconds (1 minutes)
+//     }
+// }
+
+// using Axios
+
+// const fetchTodos = async () => {
+//     const response = await axios.get("https://www.website.com")
+//     return response.data;
+// }

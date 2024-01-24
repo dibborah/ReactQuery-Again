41fff382a5f870022b69ec1e9ab2c5f5ce8e862a
dibborah <dibyajyotiborah469@gmail.com>
2024-01-24 23:01:44 +0530

    useMutation just slight notes

---
+++ src/notes5.js
@@ -0,0 +1,9 @@
+// Mutations in Tanstack React Query
+
+// Unlike queries, 
+// Mutations are typically used to CREATE/UPDATE/DELETE data (server side-effects)
+// Or perform server side-effects.
+
+// For this purpose, Tanstack Query Provides Us:
+// useMutation hook
+
